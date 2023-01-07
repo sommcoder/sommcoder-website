0d1365f1fb067a0492a5db2e0f2f31141bfd1fd0
Brian Davies <brian.davies589@gmail.com>
2023-01-07 12:46:27 -0500

    finally got the image asset to properly import, turns out it is not the same as using WebPack, with Vite you can use Import and add that imported asset to the img's src attribute

--- sommcoder-website/src/components/IntroImage/IntroImage.jsx
+++ sommcoder-website/src/components/IntroImage/IntroImage.jsx
@@ -1,20 +1,27 @@
 ﻿import styled from "styled-components";
+import headshot from "../../assets/headshot.jpeg";
 
 export default function IntroImage() {
   return (
-    <StyledIntroImage
-      // alt="Brian Davies Headshot"
-      src="../../assets/headshot.jpeg"
-    />
+    <StyledIntroImageBox>
+      <img image alt="Brian Davies Headshot" src={headshot} />
+    </StyledIntroImageBox>
   );
 }
-const StyledIntroImage = styled.img`
+const StyledIntroImageBox = styled.div`
   justify-self: right;
-  clip-path: circle(85%);
   border: 2px solid #bba6d2;
-  width: 15rem;
-  height: 15rem;
+  max-width: 20rem;
+  height: auto;
   border-radius: 50%;
+  align-self: center;
+  overflow: hidden;
+
+  img {
+    justify-self: right;
+    max-width: 20rem;
+    align-self: center;
+  }
 
   // when the screen is sub 500px, we will wrap this component to be adjacent to teh IntroDescription component and display: none on the HeroGraphic (completed)
 `;
